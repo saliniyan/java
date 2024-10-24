a23a00837754b1261efbc31f67bd0514a5584e39
saliniyan <saliniyanp02@gmail.com>
2024-10-24 22:13:41 +0530

    server code

--- server.java
+++ server.java
@@ -1,6 +1,28 @@
+interface Servlet{
+    void service();
+}
 class server{
-    public static void main(String args[])
+    public static void main(String args[]) throws Exception
     {
+        Object obj = Class.forName(args[0]).newInstance();
+        //obj.service();
+        Servlet servlet = (Servlet)obj;
+        servlet.service();
+    }
+}
 
+//developer 1
+class Loginservlet implements Servlet{
+    public void service()
+    {
+        System.out.println("This is Login Servlet");
+    }
+}
+
+//developer 2
+class Registration implements Servlet{
+    public void service()
+    {
+        System.out.println("This is Regitration Servlet");
     }
 }
